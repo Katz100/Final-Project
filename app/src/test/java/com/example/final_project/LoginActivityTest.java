0ccb52777f9af50669e42e38a4196909918df495
Katz100 <cody.hopkins108@gmail.com>
2025-08-02 15:41:31 -0700

    Add dependencies and tests for login page

---
+++ app/src/test/java/com/example/final_project/LoginActivityTest.java
@@ -0,0 +1,53 @@
+package com.example.final_project;
+import static org.junit.Assert.assertEquals;
+import static org.robolectric.Shadows.shadowOf;
+
+import android.content.Intent;
+import android.widget.EditText;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.android.controller.ActivityController;
+
+@RunWith(RobolectricTestRunner.class)
+public class LoginActivityTest {
+
+    @Test
+    public void clickingOnSignUpButton_LaunchesSignUpActivity() {
+        try (ActivityController<LoginActivity> controller = Robolectric.buildActivity(LoginActivity.class)) {
+            controller.setup();
+
+            LoginActivity activity = controller.get();
+            activity.findViewById(R.id.signUpButton).performClick();
+
+            Intent expectedIntent = new Intent(activity, SignUpActivity.class);
+            Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+            assertEquals(expectedIntent.getComponent(), actual.getComponent());
+        }
+    }
+
+    // This will need to be changed once we are comparing the values
+    // in the text fields with users from the database
+    @Test
+    public void signingInWithValidCreds_LaunchesMainActivity() {
+        try (ActivityController<LoginActivity> controller = Robolectric.buildActivity(LoginActivity.class)) {
+            controller.setup();
+            LoginActivity activity = controller.get();
+
+            EditText usernameField = activity.findViewById(R.id.usernameLoginEditText);
+            EditText passwordField = activity.findViewById(R.id.passwordLoginEditText);
+
+            usernameField.setText("username");
+            passwordField.setText("password");
+
+            activity.findViewById(R.id.loginButton).performClick();
+
+            Intent expectedIntent = new Intent(activity, MainActivity.class);
+            Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+            assertEquals(expectedIntent.getComponent(), actual.getComponent());
+        }
+    }
+}
