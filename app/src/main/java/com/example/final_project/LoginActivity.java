aaa64cbcc76f9e5206631719284a83dd1232aba6
Cody Hopkins <105841281+Katz100@users.noreply.github.com>
2025-08-03 16:25:41 -0700

    Update LoginActivity.java

--- app/src/main/java/com/example/final_project/LoginActivity.java
+++ app/src/main/java/com/example/final_project/LoginActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 import com.example.final_project.databinding.ActivityLoginBinding;
 
 /**
