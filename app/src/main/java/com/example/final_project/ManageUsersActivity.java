ac13c9e605f5e54a0c1e5e7d051a327a2eacf36c
Mariah Stinson <mstinson@csumb.edu>
2025-08-03 14:15:46 -0700

    added back button to manage users, added visible back button and text indicating which page its going back to (admin dashboard).

--- app/src/main/java/com/example/final_project/ManageUsersActivity.java
+++ app/src/main/java/com/example/final_project/ManageUsersActivity.java
@@ -19,10 +19,23 @@ public class ManageUsersActivity extends AppCompatActivity {
         binding = ActivityManageUsersBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //adds back button to action bar
+        setSupportActionBar(binding.usersToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         // Set up the UI components and listeners for managing users
         // RecyclerView, buttons, etc. can be initialized here
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        Intent intent = new Intent(this, AdminActivity.class);
+        startActivity(intent);
+        finish();
+        return true;
+    }
     static Intent manageUsersIntentFactory(Context context) {
         return new Intent(context, ManageUsersActivity.class);
     }
