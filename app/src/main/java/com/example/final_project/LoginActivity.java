e8c1c0e0d800cb3ba82b6ae74c3b76c927ab4f5c
Cody Hopkins <105841281+Katz100@users.noreply.github.com>
2025-08-03 16:24:25 -0700

    revert changes to login activity

--- app/src/main/java/com/example/final_project/LoginActivity.java
+++ app/src/main/java/com/example/final_project/LoginActivity.java
@@ -3,18 +3,11 @@ package com.example.final_project;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-
-import com.example.final_project.database.MovieWatchlistDatabase;
-import com.example.final_project.database.WatchListRepository;
-import com.example.final_project.database.entities.Movie;
-import com.example.final_project.database.entities.User;
 import com.example.final_project.databinding.ActivityLoginBinding;
-import com.example.final_project.viewHolder.WatchListViewModel;
 
 /**
  * This is a simple login activity that allows users to log in with their username and password.
@@ -28,34 +21,12 @@ public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
     //private User user = null;
 
-    WatchListViewModel viewModel;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-
-        viewModel = new ViewModelProvider(this).get(WatchListViewModel.class);
-
-        viewModel.user.observe(this, user -> {
-            if (user != null) {
-                Toast.makeText(getApplication(), user.getUsername(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        viewModel.getUserByUsername("admin1");
-
-        new Thread(() -> {
-            MovieWatchlistDatabase db = MovieWatchlistDatabase.getDatabase(this);
-            db.movieDAO().insert(new Movie("Blade Runner", "Sci-Fi"));
-            User user = WatchListRepository.getRepository(getApplication()).getUserByUsername("admin1");
-            if (user != null) {
-                Log.i(MainActivity.TAG, user.getUsername());
-            }
-        }).start();
-
+        
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
