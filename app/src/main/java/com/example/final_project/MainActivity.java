fe16b05825d50b7e7c491f39808793b1436dd8f3
dockman20006 <justinmartlock213@gmail.com>
2025-08-04 15:03:38 -0700

    Fixing conflicts

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.final_project;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
 
 import com.example.final_project.database.WatchListRepository;
 import com.example.final_project.database.entities.Movie;
