5bd0bded2ca14e1671af0fae12c5878a08477e9f
Mariah Stinson <mstinson@csumb.edu>
2025-08-09 22:11:24 -0700

    fixed "Logout?" message for AdminActivity.java

--- app/src/main/java/com/example/final_project/AdminActivity.java
+++ app/src/main/java/com/example/final_project/AdminActivity.java
@@ -82,9 +82,9 @@ public class AdminActivity extends AppCompatActivity {
     //Afer a user confirms they want to logout, it takes them back to the sign in screen
     private void showLogoutDialog(){
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(AdminActivity.this);
-        final AlertDialog alertDialog = alertBuilder.create();
+        //final AlertDialog alertDialog = alertBuilder.create();
 
-        alertDialog.setMessage("Logout?");
+        alertBuilder.setMessage("Logout?");
         alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
 
             @Override
@@ -96,7 +96,7 @@ public class AdminActivity extends AppCompatActivity {
         alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                alertDialog.dismiss();
+                dialog.dismiss();
             }
         });
         alertBuilder.create().show();
