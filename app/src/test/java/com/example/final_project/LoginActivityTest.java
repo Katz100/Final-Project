0842f7a78441adf83c750168ddee9b7e7da0ef64
Claudia <cfierro@csumb.edu>
2025-08-10 22:38:36 -0700

    Rearranged for visibility. Removed unused imports.
    TODO: Test that a successful sign in launches main activity for reg users and admin activity for admin users

--- app/src/test/java/com/example/final_project/LoginActivityTest.java
+++ app/src/test/java/com/example/final_project/LoginActivityTest.java
@@ -6,7 +6,6 @@ import static org.robolectric.Shadows.shadowOf;
 
 import android.content.Intent;
 import android.os.Looper;
-import android.util.Log;
 import android.widget.EditText;
 
 import com.example.final_project.SignIn.TestSignInViewModel;
@@ -87,11 +86,31 @@ public class LoginActivityTest {
         }
     }
 
+    @Test
+    public void signingInWithBlankFields_DoesNotLaunchMainActivity() {
+        try (ActivityController<LoginActivity> controller = Robolectric.buildActivity(LoginActivity.class)) {
+            controller.setup();
+            LoginActivity activity = controller.get();
+
+            EditText usernameField = activity.findViewById(R.id.usernameLoginEditText);
+            EditText passwordField = activity.findViewById(R.id.passwordLoginEditText);
+
+            usernameField.setText("");
+            passwordField.setText("");
+
+            activity.findViewById(R.id.loginButton).performClick();
+
+            Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+            assertNull(actual);
+        }
+    }
+
+
 
     // This will need to be changed once we are comparing the values
     // in the text fields with users from the database
 
-//    //TODO: Complete a passing test
+//    TODO: Complete a passing test
 //    @Test
 //    public void signingInWithValidCreds_LaunchesMainActivity() {
 //        try (ActivityController<LoginActivity> controller = Robolectric.buildActivity(LoginActivity.class)) {
@@ -116,25 +135,6 @@ public class LoginActivityTest {
 //            assertEquals(expectedIntent.getComponent(), actual.getComponent());
 //        }
 //    }
-
-    @Test
-    public void signingInWithBlankFields_DoesNotLaunchMainActivity() {
-        try (ActivityController<LoginActivity> controller = Robolectric.buildActivity(LoginActivity.class)) {
-            controller.setup();
-            LoginActivity activity = controller.get();
-
-            EditText usernameField = activity.findViewById(R.id.usernameLoginEditText);
-            EditText passwordField = activity.findViewById(R.id.passwordLoginEditText);
-
-            usernameField.setText("");
-            passwordField.setText("");
-
-            activity.findViewById(R.id.loginButton).performClick();
-
-            Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
-            assertNull(actual);
-        }
-    }
 
     //TODO: Complete a passing test
 
