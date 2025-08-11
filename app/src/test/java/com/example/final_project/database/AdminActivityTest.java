81c99b0d2d673a8b4fc3bd4a1644ee016397036d
Claudia <cfierro@csumb.edu>
2025-08-11 15:48:00 -0700

    Added promote and demote methods in UserDAO. Added unit tests to UserDAOTest for promoting and demoting. All tests pass.

---
+++ app/src/test/java/com/example/final_project/database/AdminActivityTest.java
@@ -0,0 +1,18 @@
+package com.example.final_project.database;
+
+import org.junit.Test;
+
+public class AdminActivityTest {
+    @Test
+    public void testPromoteButtonUpdatesUser() {
+        // Launch activity, click promote button, verify user is promoted
+    }
+
+    @Test
+    public void testDemoteButtonUpdatesUser() {
+        // Launch activity, click demote button, verify user is promoted
+    }
+
+
+
+}
